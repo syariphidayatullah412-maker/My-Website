12d56d35d7b1627cc0f45807c0f5ebae64f437c8
syariphidayatullah412-maker <syariphidayatullah412@gmail.com>
2026-07-12 14:45:23 +0800

    Update Pengumuman.js

--- Pengumuman.js
+++ Pengumuman.js
@@ -1,13 +1,27 @@
 const pengumuman = {
-judul: "Ujian Akhir Semester (UAS)",
+
+judul: "📢 Jadwal Ujian Akhir Semester (UAS)",
 
 tanggal: "20 Juli 2026",
 
-isi: `Pelaksanaan Ujian Akhir Semester
-Fiqih & Ushul Fiqih akan dilaksanakan
-secara tatap muka di ruang kelas.`,
+isi: `
+Pelaksanaan Ujian Akhir Semester Mata Kuliah
+Fiqih & Ushul Fiqih akan dilaksanakan pada
+hari Senin, 20 Juli 2026 pukul 08.00 WITA.
+
+Mahasiswa diwajibkan:
+
+✔ Hadir 15 menit sebelum ujian dimulai.
+
+✔ Membawa KTM.
+
+✔ Berpakaian rapi dan sopan sesuai ketentuan kampus.
+
+✔ Tidak diperkenankan mengikuti ujian apabila terlambat lebih dari 30 menit.
+`,
 
 update: "12 Juli 2026"
+
 };
 
 document.getElementById("judul").innerHTML = pengumuman.judul;
